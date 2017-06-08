b79347be33c1bb1b0779a69beade923439f9592a
Tomasz Macisowicz <tmacisowicz@gmail.com>
2017-06-08 23:54:52 +0200

    dodałem kontakt, godziny otwarcia oraz mapę google jeszcze bez współrzędnych

--- script.js
+++ script.js
@@ -1,15 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-//function scroll() {
-//
-//    scroll.location.hash = 'back';
-//
-//
-//}
-//;
-
-
+function myMap() {
+    var mapProp = {
+        center: new google.maps.LatLng(51.508742, -0.120850),
+        zoom: 12,
+    };
+    var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+}
