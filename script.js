4851dba51a49c12af1e0e49156758faac0eb2a7f
Tomasz Macisowicz <tmacisowicz@gmail.com>
2017-06-09 12:17:54 +0200

    kilka nowych zdjęć i animowane przewijanie.

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+
+/* global google */
+
 function myMap() {
     var mapProp = {
         center: new google.maps.LatLng(51.508742, -0.120850),
@@ -5,3 +8,15 @@ function myMap() {
     };
     var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
 }
+
+$(document).ready(function () {
+    $('a[href^="#"]').click(function (e) {
+        e.preventDefault();
+        var target = this.hash, $target = $(target);
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top
+        }, 1500, 'swing', function () {
+            window.location.hash = target;
+        });
+    });
+});
